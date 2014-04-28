f7173c1ae690fcdcf1517c746d2508c62841c31b
Rosie Brigham and Rob Kemp <rosie.brigham+rob.kemp@fundingcircle.com>
2014-04-28 15:08:09 +0100

    edited and added tests to include devise authentication

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -2,27 +2,43 @@ require 'spec_helper'
 
 feature "Events" do
   
-  scenario "veiwing a list of events" do
+  scenario "Should have an index of events" do
     event = FactoryGirl.create(:event)
     visit '/events'
     
     page.should have_content event.name
-
   end
 
   context "when logged in" do
-    # create a factory girl user
-    # login_as(user)
+  
+  before do
+    user = FactoryGirl.create(:user)
+    login_as(user)
   end
 
-  scenario "when editing a event" do
-    # edit-y stuff with factories and tings
-  end
-  
-  scenario "when deleting a event" do
+    scenario "when editing a event" do
+      @event= FactoryGirl.create(:event)
+      visit "/events/#{@event.id}"
+      click_link 'Edit event'
+      fill_in "Name", :with => "Labyrinth"
+      click_button 'Update' 
+      page.should have_content "Labyrinth"
+    end
+    
+    scenario "when deleting a event" do
+      @event = FactoryGirl.create(:event)
+      visit "/events/#{@event.id}"
+      click_button 'Delete Event'
+      expect(page).to have_text("That event has been deleted")
+    end
   end
 
   context "when not logged in" do
+    scenario "asking for log in" do
+      visit "/events"
+      click_link 'Add an event'
+      expect(page).to have_text("You need to sign in or sign up before continuing.")
+    end
   end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "validations" do
+    it "validates the presence of a event name" do
+      subject.should have(1).error_on(:name)
+    end
+
+    it "validate presence of a genre" do
+      subject.should have(1).error_on(:genre)
+    end
+
+    it "validate presence of a artist" do
+      subject.should have(1).error_on(:artist)
+    end
+
+    it "validate presence of a venue" do
+      subject.should have(1).error_on(:venue)
+    end
+
+    it "validate presence of a date" do
+      subject.should have(1).error_on(:date)
+    end
+
+    it "validate presence of a time" do
+      subject.should have(1).error_on(:time)
+    end    
+
+  end
 end
