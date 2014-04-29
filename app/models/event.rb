781d9bdee27deb43ff4ee42f29a2c68c27d01330
Rosie Brigham and Rob Kemp <rosie.brigham+rob.kemp@fundingcircle.com>
2014-04-29 18:06:02 +0100

    added validations for user email and event date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,16 @@
+# require 'Date'
+
 class Event < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
   validates :genre, presence: true
   validates :artist, presence: true
-  validates :date, presence: true
+  validates :date, presence: true 
   validates :time, presence: true
   validates :venue, presence: true
+  validate :date_cannot_be_in_past
+  validate :must_be_valid_date
 
 
   def self.search(search)
@@ -17,4 +21,18 @@ class Event < ActiveRecord::Base
     end
   end
 
+private
+
+  def date_cannot_be_in_past
+    if date < Date.today
+      errors.add(:date, "can't be in the past") 
+    end    
+  end
+
+  def must_be_valid_date
+    if !date.is_a?(Date)
+      errors.add(:started_at, 'must be a valid date') 
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  # validates :password, :length => {:minimum => 6}, :confirmation => true
 end
