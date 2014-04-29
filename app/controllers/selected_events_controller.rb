6d9a062d2ace034298740ea8d759631ccb8fd2e8
Rosie Brigham and Rob Kemp <rosie.brigham+rob.kemp@fundingcircle.com>
2014-04-29 18:05:13 +0100

    sorted out selected events feed to /only/ display the users events

--- app/controllers/selected_events_controller.rb
+++ app/controllers/selected_events_controller.rb
@@ -2,6 +2,12 @@ class SelectedEventsController < ApplicationController
 
   def index
     @selected_events = SelectedEvent.all
+    @selected_events.each do |event|
+      if event.user_id == current_user.id
+        @selected_event_for_user = event
+      end
+      
+    end
   end
 
   def create
@@ -11,10 +17,5 @@ class SelectedEventsController < ApplicationController
     @selected_event.save
     redirect_to selected_events_path
   end
-
-  def new
-      @selected_event = SelectedEvents.new
-  end
-
 
 end
