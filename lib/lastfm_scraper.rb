46ce28bac91481999129d89f64e7a757b2250250
Rosie Brigham and Rob Kemp <rosie.brigham+rob.kemp@fundingcircle.com>
2014-05-12 14:32:20 +0100

    created scraper

--- lib/lastfm_scraper.rb
+++ lib/lastfm_scraper.rb
@@ -1,3 +1,56 @@
 class LastfmScraper
+  URL = "http://www.last.fm/events"
 
+  def self.scrape
+    @doc = Nokogiri::HTML(open(URL))
+
+    @doc.css("table.eventsMedium tr .detail a").each do |node|
+      event = Event.new
+      @eventpath = node.first[1]
+   
+      # now... we need to visit each of the individual sites and scrape the info from there to!
+      eventurl = "http://www.last.fm#{@eventpath}"
+      eventdoc = Nokogiri::HTML(open(eventurl))
+
+      # gets the title/artist
+      artist_name = eventdoc.css(".page-head.without-crumbtrail h1 a").text
+
+      if artist_name == ""
+        artist_name = eventdoc.css(".page-head.without-crumbtrail h1").text
+      end
+
+      event.artist = artist_name
+      event.name = artist_name
+
+      # gets the venue
+      eventdoc.css("p.venue a").each do |node|
+        event.venue = node.text
+      end
+
+        # getting the description
+      long_description = eventdoc.css(".wiki-text").text
+      if long_description
+        event.description = long_description.split('http').first
+      end
+
+      # getting the venue website
+      eventdoc.css("#venue a.url").each do |node|
+        event.url = node.attributes["href"].value
+      end
+
+      # getting the event image url
+      event.photo_url = eventdoc.css("#smallPoster img").first.attributes['src'].value
+
+      # getting the atetime object
+      @last_datetime = eventdoc.css("abbr.dtstart").first.attributes['title']
+      date, time = @last_datetime.text.split('T')
+      event.date = Date.parse(date)
+      event.time = DateTime.parse(time)
+      
+      event.genre = 'Concert'
+      event.save!
+      puts "just saved #{event.name} on at #{event.date}:#{event.time.hour}"
+     end
+   end
 end
+
