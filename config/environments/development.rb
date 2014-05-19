d7389879c612d031ed7fa24151806a5f98990716
Rosie Brigham <rosie.brigham@fundingcircle.com>
2014-05-19 17:58:06 +0100

    adds smtp settings for email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ NiteLife::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
+ 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,15 @@ NiteLife::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.action_mailer.default_url_options = { :host => "http://www.http://theres-alot-of-nitelife.herokuapp.com/events" }
+  ActionMailer::Base.smtp_settings = {
+  :address              => 'smtp.gmail.com',
+  :port                 => 587,
+  :domain               => "example.com",
+  :user_name            => ENV['GMAIL_USER_NAME']
+  :password             => ENV['GMAIL_PASSWORD']
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
