3ea731a2bf103c2b90cd1791f54ead1471a087c1
Rosie Brigham <rosie.brigham@fundingcircle.com>
2014-05-19 18:12:23 +0100

    stmp postmark settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,15 +19,17 @@ NiteLife::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.action_mailer.default_url_options = { :host => "http://www.http://theres-alot-of-nitelife.herokuapp.com/events" }
-  # ActionMailer::Base.smtp_settings = {
-  # :address              => 'smtp.gmail.com',
-  # :port                 => 587,
-  # :domain               => "example.com",
-  # :user_name            => ENV['GMAIL_USER_NAME']
-  # :password             => ENV['GMAIL_PASSWORD']
-  # :authentication       => 'plain',
-  # :enable_starttls_auto => true  }
+  config.action_mailer.default_url_options = { :host => "http://theres-alot-of-nitelife.herokuapp.com/events" }
+  ActionMailer::Base.smtp_settings = {
+    :port           => '25',
+    :address        => ENV['POSTMARK_SMTP_SERVER'],
+    :user_name      => ENV['POSTMARK_API_KEY'],
+    :password       => ENV['POSTMARK_API_KEY'],
+    :domain         => 'theres-alot-of-nitelife.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
