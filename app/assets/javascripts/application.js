ff703e514ce9e0fe5b9eb206f4cdb1cc0df093bb
Rosie Brigham <rosie.brigham@fundingcircle.com>
2014-05-19 12:24:59 +0100

    sorts out js trees and what order they are called in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 // 
-//= require raptorize
-//= require raptorize-konami
+
 //= require zeroclipboard
-//= require "./asset-path"
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
 
