c0188176a8ff6a0393aad8441a537f13870abafd
Rosie Brigham <rosie.brigham@fundingcircle.com>
2014-05-20 12:57:34 +0100

    adds overlooked create tests for SE

--- spec/controllers/selected_events_controller_spec.rb
+++ spec/controllers/selected_events_controller_spec.rb
@@ -4,6 +4,8 @@ describe SelectedEventsController do
 
   describe "when logged in" do
     let(:user) {FactoryGirl.create(:user)}
+    let!(:selected_event) {FactoryGirl.create(:selected_event)}
+    let(:event) { selected_event.event }
 
     before do
       sign_in user
@@ -17,6 +19,23 @@ describe SelectedEventsController do
         end
       end
     end
+
+    describe "POST#create" do
+      it "save a new selected_event" do
+          expect {
+            post :create, event_id: event
+          }.to change{ SelectedEvent.count}.by(1)       
+        end
+    end
+
+    describe "DELETE #destroy" do
+      # let!(:selected_event) { FactoryGirl.create(:selected_event) }
+      it 'will remove a selected event' do
+        expect {
+            delete :destroy, id: selected_event.id
+          }.to change{SelectedEvent.count}.from(1).to(0)
+      end
+    end
   end
 
   describe "when not logged on" do
@@ -26,15 +45,5 @@ describe SelectedEventsController do
         response.should redirect_to events_path
       end
     end
-  end
-
-  describe "DELETE #destroy" do
-    let!(:selected_event) { FactoryGirl.create(:selected_event) }
-    it 'will remove a selected event' do
-      expect {
-          delete :destroy, id: selected_event.id
-        }.to change{SelectedEvent.count}.from(1).to(0)
-    end
-  end
-
+  end  
 end
