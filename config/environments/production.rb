71bcea706f123fbb9f9e3fabfaf20281247067a3
Rosie Brigham <rosie.brigham@fundingcircle.com>
2014-05-20 16:39:52 +0100

    testing config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ NiteLife::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+  # config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
   
   config.assets.compile = true
 
